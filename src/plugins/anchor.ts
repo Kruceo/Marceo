690ef3873fa0d343cfc224382fe0df00a548eb3a
kruceo <kruceo.com@gmail.com>
2024-03-01 15:09:36 -0300

    fix: fix to anchor plugin

--- src/plugins/anchor.ts
+++ src/plugins/anchor.ts
@@ -1,7 +1,9 @@
 import Plugin from "../lib/Plugin"
 
-function anchorElement(start:string,content:string) {
-    return `<a class="markdown anchor" href="${content}">${start.slice(1, start.length - 2)}</a>`
+function anchorElement(start:string,content:string,end:string) {
+    const label = content
+    const url = end.slice(2,end.length-1)
+    return `<a class="markdown anchor" href="${url}">${label}</a>`
 }
 
-export const anchor = new Plugin(/(?<!\!)(\[.+?\])\(/, /.+?/, /\)/, 'anchor', anchorElement)
+export const anchor = new Plugin(/\[/, /.+?/, /\]\(.*?\)/, 'anchor', anchorElement)
