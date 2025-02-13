12b3ffd1dada6b3a9b098ff576e6a7fc48b415cc
kruceo <kruceo.com@gmail.com>
2025-02-13 17:57:17 -0300

    fix: big numbered lists bug fix

--- src/plugins/dotlist.ts
+++ src/plugins/dotlist.ts
@@ -4,7 +4,7 @@ function dotlistElement(start:string,content:string,last:string){
     // return "({\n" + start + content + "\n})"
     const all = "\n"+(start + content).trim()
     let result = all
-    let ma = all.match(/\n((-|\d\.) .+)/g)
+    let ma = all.match(/\n((-|\d+?\.) .+)/g)
     if(ma){
         if(/\n\d\. /.test(ma[0])){
             result = "\n<ol>" + result +"</ol>\n"
@@ -12,16 +12,16 @@ function dotlistElement(start:string,content:string,last:string){
         else result = "\n<ul>" + result + "</ul>\n"
 
         ma.forEach(matched=>{
-            result = result.replace(matched,`\n<li>${matched.substring(3)}</li>`)
+            result = result.replace(matched,`\n<li>${matched.replace(/^(\n\d+?\. )|(\n- )/m,"")}</li>`)
         })
     }
     let spaces = 4
-    let customRegex = ()=>RegExp(`\n( ){${spaces}}(-) `,'g')
+    let customRegex = ()=>RegExp(`\n( ){${spaces}}(-|\\d+?\\.) `,'g')
 
     while(result.match(customRegex())){
-        result = result.replace(customRegex(),"\n- ")
+        result = result.replace(customRegex(),(s)=>s.includes("-")?"\n- ":"\n1. ")
         spaces+=4
-        if(spaces>20)break;
+        if(spaces>100)break;
         let identified = dotlist.identifyText(result)
         let replaced = dotlist.replaceSymbols(identified)
         result = replaced
@@ -32,6 +32,6 @@ function dotlistElement(start:string,content:string,last:string){
 
 export const dotlist = new Plugin(
     /(\n)*?/,    
-    /(\n( )*(-|\d\.) .+)+/,
-    /\n(\ )*(?!-)(?!\d\.)/, 
+    /(\n( )*(-|\d+?\.) .+)+/,
+    /\n(\ )*(?!-)(?!\d+?\. )/, 
     'dotlist',dotlistElement )
